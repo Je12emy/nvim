d8ec77e7ad0a585d1f29ffe2dd58a5f4b75ae988
Je12emy <Jeremyzelaya@hotmail.es>
2021-09-19 16:47:09 -0600

    Move Packer bootstrap function

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,14 +1,4 @@
 -- ------------------------
--- -      PACKER          -   
--- ------------------------
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  vim.cmd 'packadd packer.nvim'
-end
-
--- ------------------------
 -- -      PLUGINS         -   
 -- ------------------------
 require('packer').startup(function()
@@ -18,7 +8,7 @@ require('packer').startup(function()
     use {
       'kyazdani42/nvim-tree.lua',
       requires={
-        {'kyazdani42/nvim-web-devicons'},
+        'kyazdani42/nvim-web-devicons',
       },
       'airblade/vim-rooter'
     }
