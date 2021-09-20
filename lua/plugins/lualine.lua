225fd5932d52d7025a38336a615ffaeba4138a99
Je12emy <Jeremyzelaya@hotmail.es>
2021-09-19 23:36:27 -0600

    Hide lualine on tree explorer

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,7 +1,8 @@
 local options = {
       theme = 'gruvbox-flat',
       section_separators = {'', ''},
-      component_separators = {'', ''}
+      component_separators = {'', ''},
+      disabled_filetypes = {'NvimTree'}
 }
 
 require('lualine').setup({
