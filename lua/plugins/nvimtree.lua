5576594b9eb669d7326cd15f9e6aa92b893758df
Je12emy <Jeremyzelaya@hotmail.es>
2021-09-30 10:04:38 -0600

    Fix plugin nvimtree setup and config

--- lua/plugins/nvimtree.lua
+++ lua/plugins/nvimtree.lua
@@ -2,13 +2,12 @@ local tree_cb = require'nvim-tree.config'.nvim_tree_callback
 local g = vim.g
 
 g.nvim_tree_disable_default_keybindings = 1
-
-g.nvim_tree_bindings = {
+local list = {
       { key = {"<CR>", "o", "<2-LeftMouse>"}, cb = tree_cb("edit") },
       { key = {"<2-RightMouse>", "<C-]>", "<C-d>"},    cb = tree_cb("cd") },
       { key = "<C-v>",                        cb = tree_cb("vsplit") },
       { key = "<C-x>",                        cb = tree_cb("split") },
-      { key = "<ot>",                         cb = tree_cb("tabnew") },
+      { key = "ot",                         cb = tree_cb("tabnew") },
       { key = "<",                            cb = tree_cb("prev_sibling") },
       { key = ">",                            cb = tree_cb("next_sibling") },
       { key = "P",                            cb = tree_cb("parent_node") },
@@ -38,58 +37,81 @@ g.nvim_tree_bindings = {
       { key = "g?",                           cb = tree_cb("toggle_help") },
 }
 
-vim.o.termguicolors = true
+-- following options are the default
+require'nvim-tree'.setup {
+  -- disables netrw completely
+  disable_netrw       = true,
+  -- hijack netrw window on startup
+  hijack_netrw        = true,
+  -- open the tree when running this setup function
+  open_on_setup       = true,
+  -- will not open on setup if the filetype is in this list
+  ignore_ft_on_setup  = {},
+  -- closes neovim automatically when the tree is the last **WINDOW** in the view
+  auto_close          = false,
+  -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+  open_on_tab         = false,
+  -- hijacks new directory buffers when they are opened.
+  update_to_buf_dir   = true,
+  -- hijack the cursor in the tree to put it at the start of the filename
+  hijack_cursor       = true,
+  -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually) 
+  update_cwd          = false,
+  -- show lsp diagnostics in the signcolumn
+  lsp_diagnostics     = true,
+  -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+  update_focused_file = {
+    -- enables the feature
+    enable      = true,
+    -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+    -- only relevant when `update_focused_file.enable` is true
+    update_cwd  = false,
+    -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+    -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+    ignore_list = {}
+  },
+  -- configuration options for the system open command (`s` in the tree by default)
+  system_open = {
+    -- the command to run this, leaving nil should work in most cases
+    cmd  = nil,
+    -- the command arguments as a list
+    args = {}
+  },
+
+  view = {
+    -- width of the window, can be either a number (columns) or a string in `%`
+    width = 30,
+    -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+    side = 'right',
+    -- if true the tree will resize itself after opening a file
+    auto_resize = false,
+    mappings = {
+      -- custom only false will merge the list with the default mappings
+      -- if true, it will only use your list to set the mappings
+      custom_only = false,
+      -- list of mappings to set on the tree manually
+      list = list
+    }
+  }
+}
+
 
 g.nvim_tree_add_trailing = 0 -- append a trailing slash to folder names
 g.nvim_tree_allow_resize = 1
-g.nvim_tree_auto_close = 0 -- closes tree when it's the last window
-g.nvim_tree_auto_ignore_ft = { "dashboard" } -- don't open tree on specific fiypes.
-g.nvim_tree_auto_open = 1
 g.nvim_tree_disable_netrw = 1
-g.nvim_tree_follow = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_gitignore = 1
 g.nvim_tree_hide_dotfiles = 0
 g.nvim_tree_highlight_opened_files = 0
-g.nvim_tree_hijack_netrw = 0
+g.nvim_tree_hijack_netrw = 1
 g.nvim_tree_indent_markers = 1
-g.nvim_tree_ignore = { ".git", "node_modules", ".cache" }
+g.nvim_tree_ignore = { ".git", ".cache" }
 g.nvim_tree_quit_on_open = 0 -- closes tree when file's opened
 g.nvim_tree_root_folder_modifier = table.concat { ":t:gs?$?/..", string.rep(" ", 1000), "?:gs?^??" }
-g.nvim_tree_side = "right"
-g.nvim_tree_tab_open = 0
-g.nvim_tree_update_cwd = 1
-g.nvim_tree_width = 30
-g.nvim_tree_lsp_diagnostics = 0
 
 g.nvim_tree_show_icons = {
    folders = 1,
    -- folder_arrows= 1
    files = 1,
    git = 1,
-}
-
-g.nvim_tree_icons = {
-   default = "",
-   symlink = "",
-   git = {
-      deleted = "",
-      ignored = "◌",
-      renamed = "➜",
-      staged = "✓",
-      unmerged = "",
-      unstaged = "✗",
-      untracked = "★",
-   },
-   folder = {
-      -- disable indent_markers option to get arrows working or if you want both arrows and indent then just add the arrow icons in front            ofthe default and opened folders below!
-      -- arrow_open = "",
-      -- arrow_closed = "",
-      default = "",
-      empty = "", -- 
-      empty_open = "",
-      open = "",
-      symlink = "",
-      symlink_open = "",
-   },
 }
--- lua/plugins/packer-nvim.lua
+++ lua/plugins/packer-nvim.lua
@@ -17,9 +17,11 @@ require('packer').startup(function()
     use 'wbthomason/packer.nvim'
     -- Tree Explorer
     use {
-      'kyazdani42/nvim-tree.lua',
-      'airblade/vim-rooter'
+        'kyazdani42/nvim-tree.lua',
+        requires = 'kyazdani42/nvim-web-devicons',
+        config = function() require'nvim-tree'.setup {} end
     }
+    use 'airblade/vim-rooter'
     use 'kyazdani42/nvim-web-devicons'
     -- Tab
     use 'romgrk/barbar.nvim'
