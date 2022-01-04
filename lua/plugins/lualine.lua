be14060b8a2274da3911535e006e9d163f6252b8
Jeremy Zelaya <je12emy@pop-os.localdomain>
2022-01-03 20:04:50 -0600

    Update lualine config

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,20 @@
 local options = {
-      theme = "catppuccin",
+      theme = 'catppuccin',
       disabled_filetypes = {'NvimTree'},
       section_separators = { left = '', right = ''},
-      component_separators = { left = '', right = ''}
+      component_separators = { left = '', right = ''},
 }
+
+local sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'encoding', 'fileformat', 'filetype'},
+    lualine_y = {'progress'},
+    lualine_z = {'location'}
+  },
 
 require('lualine').setup({
     options = options,
+    sections = sections
 })
