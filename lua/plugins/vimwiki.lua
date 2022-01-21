ea1e56cd3ee286ddc9c3f52f5aeca29e02ca8edc
Jeremy Zelaya <je12emy@pop-os.localdomain>
2022-01-21 14:57:05 -0600

    Configure vimwiki settings

--- lua/plugins/vimwiki.lua
+++ lua/plugins/vimwiki.lua
@@ -2,3 +2,7 @@ local g = vim.g
 
 g['vimwiki_list'] = { { path = '~/wiki', syntax = 'markdown', ext = '.md' }, 
                       { path = '~/journal', syntax = 'markdown', ext = '.md' } }
+
+g['vimwiki_toc_header'] = 'Contenido'
+g['vimwiki_auto_chdir'] = 1
+g['vimwiki_use_calendar'] = 1
