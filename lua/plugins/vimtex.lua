66df3c61c8152e3b7c567fbcc7b2e88428b5c44c
dio <dio@pop-os.localdomain>
2022-02-13 21:01:34 -0600

    Remove vimtex

--- lua/plugins/vimtex.lua
+++
@@ -1,8 +0,0 @@
--- lervag/vimtex
--- sudo apt-get install texlive-full
-vim.g["vimtext_view_method"] = 'zathura'
-vim.g["text_flavor"] = 'latex'
-vim.cmd[["
-  let g:vimtex_compiler_latexmk_engines={'_':'-xelatex'}
-  let g:vimtex_compiler_latexrun_engines={'_':'xelatex'}
-"]]
