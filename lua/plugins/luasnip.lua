d1827a601b88d83d8488483d30b61ba2d7d45c34
Jeremy Zelaya <jzelaya@krames.com>
2022-03-03 12:05:03 -0600

    Update luasnip keymaps

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -26,17 +26,17 @@ ls.config.set_config {
   },
 }
 
--- <c-k> is my expansion key
+-- <c-j> is my expansion key
 -- this will expand the current item or jump to the next item within the snippet.
-vim.keymap.set({ "i", "s" }, "<c-k>", function()
+vim.keymap.set({ "i", "s" }, "<c-j>", function()
   if ls.expand_or_jumpable() then
     ls.expand_or_jump()
   end
 end, { silent = true })
 
--- <c-j> is my jump backwards key.
+-- <c-k> is my jump backwards key.
 -- this always moves to the previous item within the snippet
-vim.keymap.set({ "i", "s" }, "<c-j>", function()
+vim.keymap.set({ "i", "s" }, "<c-k>", function()
   if ls.jumpable(-1) then
     ls.jump(-1)
   end
